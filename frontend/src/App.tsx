1c83bbbf936367e46fae93b97ddce5a49b6f5f2e
PETACCIA <petaccia.seb@gmail.com>
2023-12-11 21:07:35 +0100

    création des routes de navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Profil from './pages/Profil';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <>
-      <h1 className=""></h1>
-       
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profil" element={<Profil />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+
     </>
   )
 }
